0c9a2fe4e183cf1f3e2946f18ee54a7d4a4e32b9
Malcolm Harrow <harrow.malcolm@gmail.com>
2019-04-21 23:38:57 +0800

    Working version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # qrcode
 Small project to test a FPS qrcode
+
+Type 'npm install' to install the code then one of:
+
+npm run dev
+npm run build
+npm run deploy
+
---
+++ src/js/index.js
@@ -0,0 +1,61 @@
+import 'bootstrap';
+import '../scss/index.scss';
+
+import QrCodeWithLogo from 'qr-code-with-logo'
+import crc16ccitt from 'crc/crc16ccitt'
+
+import LocalImage from '../public/treelogo.jpg'
+ 
+function calcqr(FPSid, ccy, amount) {
+
+  const payloadFormatIndicator = '000201';
+  const pointOfInitiationMethod = '010212';
+  const globallyUniqueId = '0012hk.com.hkicl';
+  const fpsAccount = '0207' + FPSid;
+
+  const hkPaymentInfo = globallyUniqueId + fpsAccount;
+  const hkPaymentCode = '26' + hkPaymentInfo.length.toString().padStart(2, '0');
+
+  const merchantCategoryCode = '52040000';
+  const transactionCurrency = '5303' + ccy;
+  const transactionAmount = '54' + amount.length.toString().padStart(2, '0') + amount;
+
+  const countryCode = '5802HK';
+  const merchantName = '5902NA';
+  const merchantCity = '6002HK';
+  const billNo = '010898756383'; // Not sure we use this
+  const additionalInfo = '62' + billNo.length.toString().padStart(2, '0');
+  const crcValue = '6304';
+
+  const qrTempStr = payloadFormatIndicator +
+                    pointOfInitiationMethod + 
+                    hkPaymentCode +
+                    hkPaymentInfo + merchantCategoryCode +
+                    transactionCurrency +
+                    transactionAmount +
+                    countryCode + 
+                    merchantName + 
+                    merchantCity +
+                    additionalInfo +
+                    billNo +
+                    crcValue;
+
+  const crcRes = crc16ccitt(qrTempStr).toString(16);
+  const finalQRStr = qrTempStr + crcRes;
+  
+  QrCodeWithLogo.toCanvas({
+    canvas: document.getElementById('qrcanvas'),
+    content: finalQRStr,
+    width: 300,
+    errorCorrectionLevel: 'H',
+    logo: {
+      src: LocalImage,
+      logoSize: 0.15
+    }
+  })
+}
+
+// https://stackoverflow.com/questions/35781579/basic-webpack-not-working-for-button-click-function-uncaught-reference-error
+window.calcqr = calcqr;
+
+
